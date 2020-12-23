abbc45cef5cd80adcccfa78c83c50770c1a9f1b8
ehsan312 <bird.prey34@gmail.com>
2020-12-23 22:49:24 +0330

    write my def

--- app.py
+++ app.py
@@ -13,29 +13,18 @@ app = Flask(__name__)
 def main_page():
     return "Hi"
 
-@app.route('/get_sms')
+@app.route('/v1/get_sms')
 def get_sms():
     a = sms.fetch_inbox()
     b = a[0][0]
     return b.message
-
-c = get_sms()
-
-if c == "1":
-    for i in range(a):
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(18,GPIO.OUT)
-
-
-        GPIO.output(18,GPIO.LOW)
-
-        time.sleep(0.1)
+def turn_on()
+    c = get_sms()
+    if c == "1":
+        for i in range(a):
+            GPIO.setmode(GPIO.BCM)
+            GPIO.setup(18,GPIO.OUT)
+            GPIO.output(18,1)
 
 
-        GPIO.output(18,GPIO.HIGH)
-
-        time.sleep(0.1)
-
-        GPIO.output(18,GPIO.LOW)
-        GPIO.cleanup()
 
