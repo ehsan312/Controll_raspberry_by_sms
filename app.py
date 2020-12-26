823e36a690aee06c7937bbbb613baccc962250bc
ehsan312 <bird.prey34@gmail.com>
2020-12-26 20:33:22 +0330

    get sms and control raspberry

--- app.py
+++ app.py
@@ -1,33 +1,42 @@
-from flask import Flask
-import config
-import ippanel
-from ippanel import Client
+from flask import Flask, jsonify, request
 import RPi.GPIO as GPIO
-
-api_key = "api-key"
-sms = Client(config.api_keys)
-
+import flask
+import time 
 app = Flask(__name__)
 
 
-@app.route('/')
-def main_page():
-    return "Hi"
-
-@app.route('/v1/process')
+@app.route('/v1/', methods=['GET','POST'])
 def process():
-    a = sms.fetch_inbox()
-    b = a[0][0]
-    return b.message
-
-
-def turn_on():
-    c = get_sms()
-    if c == "1":
-        for i in range(a):
+    if flask.request.method == 'POST':
+        data = request.form
+        sender = data['from']
+        message = data['message']
+        print (message)
+        if  message == "1":
             GPIO.setmode(GPIO.BCM)
             GPIO.setup(18,GPIO.OUT)
-            GPIO.output(18,1)
+            GPIO.output(18, 1)
+            time.sleep(10)
+            GPIO.cleanup()
+        elif message == "2":
+            GPIO.setmode(GPIO.BCM)
+            GPIO.setup(18,GPIO.OUT)
+            GPIO.output(18,0)
+            GPIO.cleanup()
+
+        ret = {"message":"processed"}
+        return jsonify(ret), 200
+    else:
+        return 'Hi!!!!!!'
+
+
+# def turn_on():
+   #  c = process()
+   #  if c == "1":
+   #      for i in range(a):
+   #         GPIO.setmode(GPIO.BCM)
+   #         GPIO.setup(18,GPIO.OUT)
+   #         GPIO.output(18,1)
 
 
 
