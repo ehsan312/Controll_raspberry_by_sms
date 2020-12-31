08609c31a2d8bf6d4df2be7f8bec21608dab70ae
ehsan312 <bird.prey34@gmail.com>
2020-12-31 13:35:03 +0330

    notebook

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, jsonify, request
 import RPi.GPIO as GPIO
 import flask
-import time 
+import time
+import requests
+import config 
 app = Flask(__name__)
 
 
@@ -16,28 +18,31 @@ def process():
             GPIO.setmode(GPIO.BCM)
             GPIO.setup(18,GPIO.OUT)
             GPIO.output(18, 1)
-            time.sleep(10)
-            GPIO.cleanup()
+            time.sleep()
+            GPIO.cleanup(20)
+            send_sms(sender, 'Your system ON')
+            print ('your system ON')
         elif message == "2":
             GPIO.setmode(GPIO.BCM)
             GPIO.setup(18,GPIO.OUT)
             GPIO.output(18,0)
             GPIO.cleanup()
-
+            send_sms(sender, 'your system OFF')
+            print ('your system OFF')
+        elif message == 'HELP':
+            send_sms(sender, 'For turn on send number 1 /n and for turn of send number 2')
+        else:
+            send_sms(sender, 'Your number is wrong, for more information send /n HELP/n to me!!!')
         ret = {"message":"processed"}
         return jsonify(ret), 200
     else:
         return 'Hi!!!!!!'
 
-
-# def turn_on():
-   #  c = process()
-   #  if c == "1":
-   #      for i in range(a):
-   #         GPIO.setmode(GPIO.BCM)
-   #         GPIO.setup(18,GPIO.OUT)
-   #         GPIO.output(18,1)
-
+def send_sms(receptor, message):
+    url = f'https://api.kavenegar.com/v1/{config.API_KEY}/sms/send.json'
+    data = {"message": message,
+            "receptor": receptor}
+    res = requests.post(url, data)
 
 
 if __name__ == "__main__":
